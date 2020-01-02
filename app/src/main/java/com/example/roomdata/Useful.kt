804df96b3801d24204f4240ceaf605af6037de75
kohcheeyit <kohcheeyit@gmail.com>
2020-01-02 14:26:31 +0800

    lmao

---
+++ app/src/main/java/com/example/roomdata/Useful.kt
@@ -0,0 +1,12 @@
+package com.example.roomdata
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "useful")
+
+data class Useful (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    val age: Int
+    )
